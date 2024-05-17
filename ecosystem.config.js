e89ab788753685d9195ec283273e443485ad0e58
hotuns <hedongshu@foxmail.com>
2024-05-17 10:46:15 +0800

    use pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+      {
+        name: 'lurelog',
+        port: '3111',
+        exec_mode: 'cluster',
+        instances: 'max',
+        script: './.output/server/index.mjs'
+      }
+    ]
+  }
