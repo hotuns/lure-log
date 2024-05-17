948ab93b24702ceec784c49dc48e9ef724b43721
hotuns <hedongshu@foxmail.com>
2024-05-17 10:47:28 +0800

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,9 @@
-module.exports = {
-    apps: [
-      {
+export const apps = [
+    {
         name: 'lurelog',
         port: '3111',
         exec_mode: 'cluster',
         instances: 'max',
         script: './.output/server/index.mjs'
-      }
-    ]
-  }
+    }
+];
